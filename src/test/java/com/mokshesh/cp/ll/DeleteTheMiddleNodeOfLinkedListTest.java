f3f5c3864b43802be0e337e943335a7e6a4486e4
Mokshesh Dafariya <mokshesh@outlook.com>
2022-08-30 23:28:25 +0530

    feat: Delete middle node of a Linked List
    Signed-off-by: Mokshesh Dafariya <mokshesh@outlook.com>

---
+++ src/test/java/com/mokshesh/cp/ll/DeleteTheMiddleNodeOfLinkedListTest.java
@@ -0,0 +1,38 @@
+package com.mokshesh.cp.ll;
+
+import com.mokshesh.cp.ll.base.ListNode;
+import com.mokshesh.cp.ll.base.ListNodeHelper;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+class DeleteTheMiddleNodeOfLinkedListTest {
+  static DeleteTheMiddleNodeOfLinkedList deleteTheMiddleNodeOfLinkedList;
+
+  @BeforeAll
+  static void setUp() {
+    deleteTheMiddleNodeOfLinkedList = new DeleteTheMiddleNodeOfLinkedList();
+  }
+
+  static Stream<Arguments> generateData() {
+    return Stream.of(
+      Arguments.arguments(ListNodeHelper.build(new int[]{1, 3, 4, 7, 1, 2, 6}), new int[]{1, 3, 4, 1, 2, 6}),
+      Arguments.arguments(ListNodeHelper.build(new int[]{1, 2, 3, 4}), new int[]{1, 2, 4}),
+      Arguments.arguments(ListNodeHelper.build(new int[]{2, 1}), new int[]{2}),
+      Arguments.arguments(ListNodeHelper.build(new int[]{1, 2, 3, 4, 5}), new int[]{1, 2, 4, 5}),
+      Arguments.arguments(ListNodeHelper.build(new int[]{43}), new int[]{})
+    );
+  }
+
+  @ParameterizedTest
+  @MethodSource("generateData")
+  void deleteMiddle(ListNode head, int[] expectedResult) {
+    Assertions.assertThat(ListNodeHelper.toArray(deleteTheMiddleNodeOfLinkedList.deleteMiddle(head)))
+      .hasSize(expectedResult.length)
+      .containsSequence(expectedResult);
+  }
+}
