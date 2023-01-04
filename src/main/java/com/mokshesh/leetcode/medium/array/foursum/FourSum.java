83f43fc78a56f5dafb59d991ce1ca6b03af09f4f
Mokshesh Dafariya <mokshesh@outlook.com>
2023-01-05 01:45:00 +0530

    feat: add "4Sum" problem details

---
+++ src/main/java/com/mokshesh/leetcode/medium/array/foursum/FourSum.java
@@ -0,0 +1,9 @@
+package com.mokshesh.leetcode.medium.array.foursum;
+
+import java.util.List;
+
+public class FourSum {
+    public List<List<Integer>> fourSum(int[] nums, int target) {
+        return null;
+    }
+}
