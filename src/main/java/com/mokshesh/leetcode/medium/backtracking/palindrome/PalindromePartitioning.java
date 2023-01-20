f4e196d3c6d9a15cbafdf031af5928dd50b37cf9
Mokshesh Dafariya <mokshesh@outlook.com>
2023-01-20 22:45:01 +0530

    feat: add "Palindrome Partitioning" problem details

---
+++ src/main/java/com/mokshesh/leetcode/medium/backtracking/palindrome/PalindromePartitioning.java
@@ -0,0 +1,9 @@
+package com.mokshesh.leetcode.medium.backtracking.palindrome;
+
+import java.util.List;
+
+public class PalindromePartitioning {
+    public List<List<String>> partition(String s) {
+        return null;
+    }
+}
