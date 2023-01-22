cfa778f48a2f7139ab6867bc5b4fad75898f7234
Mokshesh Dafariya <mokshesh@outlook.com>
2023-01-22 21:03:48 +0530

    feat: add "LRU Cache" problem details

---
+++ src/main/java/com/mokshesh/leetcode/medium/design/lrucache/LRUCache.java
@@ -0,0 +1,15 @@
+package com.mokshesh.leetcode.medium.design.lrucache;
+
+public class LRUCache {
+    public LRUCache(int capacity) {
+        
+    }
+    
+    public int get(int key) {
+        return -1;
+    }
+    
+    public void put(int key, int value) {
+        
+    }
+}
