8f11532b56414e940b81306097faefebda922dae
Mokshesh Dafariya <mokshesh@outlook.com>
2023-01-29 20:12:00 +0530

    feat: add "Sort List" problem details

---
+++ src/main/java/com/mokshesh/leetcode/medium/linkedlist/sort/SortList.java
@@ -0,0 +1,24 @@
+package com.mokshesh.leetcode.medium.linkedlist.sort;
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+public class SortList {
+    public ListNode sortList(ListNode head) {
+        return null;
+    }
+}
