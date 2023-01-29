c9c83631f2165fe8c99fe42e659dd49d3433cc19
Mokshesh Dafariya <mokshesh@outlook.com>
2023-01-29 15:14:10 +0530

    feat: add "Reorder List" problem details

---
+++ src/main/java/com/mokshesh/leetcode/medium/linkedlist/reorder/ReorderList.java
@@ -0,0 +1,24 @@
+package com.mokshesh.leetcode.medium.linkedlist.reorder;
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+public class ReorderList {
+    public void reorderList(ListNode head) {
+        
+    }
+}
