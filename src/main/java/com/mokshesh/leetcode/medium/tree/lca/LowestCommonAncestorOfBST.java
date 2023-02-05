d783e8eb325f2ae9d26d355a6000fe50e331c1fe
Mokshesh Dafariya <mokshesh@outlook.com>
2023-02-05 06:03:54 +0530

    feat: add "Lowest Common Ancestor of a Binary Search Tree" problem details

---
+++ src/main/java/com/mokshesh/leetcode/medium/tree/lca/LowestCommonAncestorOfBST.java
@@ -0,0 +1,9 @@
+package com.mokshesh.leetcode.medium.tree.lca;
+
+import com.mokshesh.cp.helper.TreeNode;
+
+public class LowestCommonAncestorOfBST {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        return null;
+    }
+}
