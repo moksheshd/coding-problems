bba95099ecd94d366331587bd57535444fd22c85
Mokshesh Dafariya <mokshesh@outlook.com>
2023-02-05 12:16:18 +0530

    feat: add "Binary Search Tree Iterator" problem details

---
+++ src/main/java/com/mokshesh/leetcode/medium/tree/iterator/BSTIterator.java
@@ -0,0 +1,17 @@
+package com.mokshesh.leetcode.medium.tree.iterator;
+
+import com.mokshesh.cp.helper.TreeNode;
+
+public class BSTIterator {
+    public BSTIterator(TreeNode root) {
+        
+    }
+    
+    public int next() {
+        return 0;
+    }
+    
+    public boolean hasNext() {
+        return false;
+    }
+}
