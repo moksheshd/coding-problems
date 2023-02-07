1742d4845300a77f8d42517d61f514fc6d938795
Mokshesh Dafariya <mokshesh@outlook.com>
2023-02-07 22:14:09 +0530

    feat: add "Sum Root to Leaf Numbers" problem details

---
+++ src/main/java/com/mokshesh/leetcode/medium/tree/sumroot/SumRootToLeafNumbers.java
@@ -0,0 +1,20 @@
+package com.mokshesh.leetcode.medium.tree.sumroot;
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+public class SumRootToLeafNumbers {
+    public int sumNumbers(TreeNode root) {
+        return 0;
+    }
+}
